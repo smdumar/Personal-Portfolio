e37ccc7773d8b65eeadd6a6e3ed2e1dae73dc7d3
smdumar <smdumar2040@gmail.com>
2024-08-04 22:11:00 +0530

    n

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -1,4 +1,4 @@
-import SongCarousel from "./SongCarousel";
+// import SongCarousel from "./SongCarousel";
 import "../animations/animate.css";
 import AnimatedBody from "../animations/AnimatedBody";
 import AnimatedTitle from "../animations/AnimatedTitle";
@@ -100,7 +100,7 @@ const About = () => {
           </div>
         </div>
         <div className="mt-10 flex flex-col md:-mt-0 lg:mt-28">
-          <SongCarousel />
+          {/* <SongCarousel /> */}
           <AnimatedBody
             text="A few Designs I can recommend if you're looking for some fresh Designs :)"
             className="absolute bottom-10 right-0 left-0 mx-auto w-[90%] text-center text-[14px] font-semibold uppercase text-[#e4ded7] sm:w-[500px] md:bottom-12 md:w-[550px] md:text-[16px] "
