89a7da30c57b1f3bc25aee5d8812b56b62fcc544
smdumar <smdumar2040@gmail.com>
2024-08-08 12:07:27 +0530

    M

--- app/about-section/songDetails.ts
+++ app/about-section/songDetails.ts
@@ -1,69 +1,69 @@
-// export type SongProps = {
-//   title: string;
-//   artist: string;
-//   image: string;
-//   link: string;
-// };
+export type SongProps = {
+  title: string;
+  artist: string;
+  image: string;
+  link: string;
+};
 
-// export const songs = [
-//   {
-//     title: "Portfolio1",
-//     artist: "",
-//     image: require(".//../../public/songs/portfolio1.png"),
-//     link: "",
-//   },
-//   {
-//     title: "Portfolio2",
-//     artist: "",
-//     image: require(".//../../public/songs/portfolio2.png"),
-//     link: "",
-//   },
-//   {
-//     title: "Portfolio3",
-//     artist: "",
-//     image: require(".//../../public/songs/portfolio3.png"),
-//     link: "",
-//   },
-//   {
-//     title: "Portfolio4",
-//     artist: "",
-//     image: require(".//../../public/songs/portfolio4.png"),
-//     link: "",
-//   },
-//   {
-//     title: "Portfolio5",
-//     artist: "",
-//     image: require(".//../../public/songs/portfolio5.png"),
-//     link: "",
-//   },
-//   {
-//     title: "WeeGoo",
-//     artist: "",
-//     image: require(".//../../public/songs/WeeGoo.png"),
-//     link: "",
-//   },
-//   {
-//     title: "Insta Posts",
-//     artist: "",
-//     image: require(".//../../public/songs/Insta Posts.png"),
-//     link: "",
-//   },
-//   {
-//     title: "Salwa",
-//     artist: "",
-//     image: require(".//../../public/songs/Salwa.png"),
-//     link: "",
-//   },
-//   {
-//     title: "6",
-//     artist: "",
-//     image: require(".//../../public/songs/6.png"),
-//     link: "",
-//   },
-//   {
-//     title: "7",
-//     artist: "",
-//     image: require(".//../../public/songs/7.png"),
-//     link: "",
-//   },
-// ];
+export const songs = [
+  {
+    title: "Portfolio1",
+    artist: "",
+    image: require("../../public/songs/portfolio1.png"),
+    link: "",
+  },
+  {
+    title: "Portfolio2",
+    artist: "",
+    image: require("../../public/songs/portfolio2.png"),
+    link: "",
+  },
+  {
+    title: "Portfolio3",
+    artist: "",
+    image: require("../../public/songs/portfolio3.png"),
+    link: "",
+  },
+  {
+    title: "Portfolio4",
+    artist: "",
+    image: require("../../public/songs/portfolio4.png"),
+    link: "",
+  },
+  {
+    title: "Portfolio5",
+    artist: "",
+    image: require("../../public/songs/portfolio5.png"),
+    link: "",
+  },
+  {
+    title: "WeeGoo",
+    artist: "",
+    image: require("../../public/songs/WeeGoo.png"),
+    link: "",
+  },
+  {
+    title: "Insta Posts",
+    artist: "",
+    image: require("../../public/songs/Insta Posts.png"),
+    link: "",
+  },
+  {
+    title: "Salwa",
+    artist: "",
+    image: require("../../public/songs/Salwa.png"),
+    link: "",
+  },
+  {
+    title: "6",
+    artist: "",
+    image: require("../../public/songs/6.png"),
+    link: "",
+  },
+  {
+    title: "7",
+    artist: "",
+    image: require("../../public/songs/7.png"),
+    link: "",
+  },
+];
